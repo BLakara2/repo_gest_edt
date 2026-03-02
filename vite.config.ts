d60b3ae1c6cb1707d163d80092983cc0668471af
bryan-isa <bryan@isa-outsourcing.com>
2026-03-02 13:24:21 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/repo_gest_users/',
+  base: '/repo_gest_edt/',
 })
